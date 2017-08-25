97b8d9aa70e26f739286c0b425e915210d20e7a8
KLsz <klsz@outlook.com>
2017-08-25 09:17:52 +0800

    .travis.yml, main.cpp: fix the bug of compiling error;

--- Base32GUI/main.cpp
+++ Base32GUI/main.cpp
@@ -2,8 +2,8 @@
 
 //Qt's includes
 //add folder path when including, get folder path from /usr/include/x86_64-linux-gnu/qt5
-#include <QtGui/QGuiApplication>
-#include <QtQml/QQmlApplicationEngine>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 #include "../Base32/Base32.cpp"
 
